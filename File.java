e37acc99dc487286126f79a81b5c47cbc5b13ba2
author1 <author1@email.com>
2022-04-19 10:47:46 -0300

    Author1 modificou File.java adicionando um novo método de 3 linhas, mais comentário.

--- File.java
+++ File.java
@@ -2,5 +2,7 @@ public class File{
 // criando o arquivo file com 3 linhas
 public void static main(String args[]){
 }// fim main
-/* Classe de teste da ferramenta
- de extração a partir de um
+// método que exibe uma mensagem no terminal
+ public void showMsg(String msg){
+System.out.println(msg);
+}
