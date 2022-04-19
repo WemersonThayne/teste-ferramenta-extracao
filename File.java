71d1933466a091f3ff38904c740668a1316b4a74
author2 <author2@email.com>
2022-04-19 10:47:45 -0300

    Author2 modificou o File.java e adicionou 2 novas linhas

--- File.java
+++ File.java
@@ -1,5 +1,6 @@
 public class File{
 // criando o arquivo file com 3 linhas
+public void static main(String args[]){
+}// fim main
 /* Classe de teste da ferramenta
  de extração a partir de um
-repositório fictício*/
